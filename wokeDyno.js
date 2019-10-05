5455057f6928a8cab66d694ac606b1910873a15b
Dennis Hodges <fermentationist@gmail.com>
2019-10-05 00:11:02 -0500

    experiencing unreliable results with shorter intervals...

--- wokeDyno.js
+++ wokeDyno.js
@@ -4,22 +4,27 @@ const fetch = require("node-fetch");
 
 const wakeUpDyno = ({
     url, 
-    interval = 25, 
+    interval = 1.5e6, 
     startNap = [22,0,0,1], 
     endNap = [23,59,59,999], 
     callback
 }) => {
-    const milliseconds = interval * 60000;
+    const minutes = (interval / 60000).toFixed(3);
+    console.log("TCL: minutes", minutes)
     try {
-        setTimeout(() => {
+        const currentTimer = setTimeout(() => {
             try { 
-                console.log(`setTimeout called.`);
-                if (! isNaptime(startNap, endNap)){// fetch only during allowed hours
-                    fetch(url).then(() => console.log(`Fetching ${url}. Dyno is woke.`)); // HTTP GET request to the dyno's url
+                const naptime = timeToNap(startNap, endNap);
+                if (naptime){// fetch only during allowed hours
+                    console.log(`It's naptime! Napping for ${(naptime / 60000).toFixed(2)} minutes...`, naptime);
+                    currentTimer.refresh();
+                    // clearTimeout(currentTimer);
+                    return wakeUpDyno({url, interval: naptime, startNap, endNap, callback});
                 }
+                fetch(url).then(() => console.log(`Fetching ${url}. Dyno is woke. \nNext fetch request in ${minutes} minutes...`)); // HTTP GET request to the dyno's url
             }
             catch (err) { // catch fetch errors
-                console.log(`Error fetching ${url}: ${err.message} \nWill try again in ${interval} minutes...`);
+                console.log(`Error fetching ${url}: ${err.message} \nWill try again in ${minutes} minutes...`);
             }
             finally {
 
@@ -30,11 +35,12 @@ const wakeUpDyno = ({
                     callback ? console.log("Callback failed: ", e.message) : null;
                 }
                 finally {
-                    return wakeUpDyno(url, interval, startNap, endNap, callback);// do it all again
+                    currentTimer.refresh();
+                    return wakeUpDyno({url, interval, startNap, endNap, callback});// do it all again
                 }
                 
             }
-        }, milliseconds);
+        }, interval);
     }
     catch(error){
         console.log(`Error executing setTimeout: ${err.message}`);
@@ -45,23 +51,23 @@ const wakeUpDyno = ({
 If current time falls between startTime and endTime, returns length startTime and endTime are arrays of numbers representing the time of day. They follow this pattern: [Hours, Minutes, Seconds, Milliseconds]. If endTime is less than startTime time, endTime will be assumed to be on the following day. 
 
 If current time is not between startTime and endTime, will return false */
-const isNaptime = (startTime, endTime) => { 
+const timeToNap = (startTime, endTime) => { 
     const now = new Date(Date.now());
     const todayArray = [
-        now.getFullYear(),
-        now.getMonth(),
-        now.getDate()
+        now.getUTCFullYear(),
+        now.getUTCMonth(),
+        now.getUTCDate()
     ];
     const start = new Date(Date.UTC(...todayArray, ...startTime));
     const end = new Date(Date.UTC(...todayArray, ...endTime));
     const finish = start < end ? end : end.setDate(end.getDate() + 1);
-
-    //     let finish = start <= end[i] ? end[i] : end[i] + 24;// to account 
-    return now >= start && now <= finish;
+    if (now >= start && now <= finish){
+        return finish - now;
+    }
+    return false;
 }
 
 module.exports = wakeUpDyno;
 
-// wakeUpDyno("https://dennis-hodges.com", 0.125, [11,0,0,0], [5,0,0,0]);
-wakeUpDyno({url:"https://google.com", interval: 0.125});
-// console.log(isDormant([5,0,0,0], [11,0,0,0]))
+
+wakeUpDyno({url:"https://google.com", interval: 20000, startNap: [4,52,0,0], endNap: [4,54,0,666]});
